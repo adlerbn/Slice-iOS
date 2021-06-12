86900ce611215c71d257d5568d7b9e1e5b51fd27
bakhtiarniayahya1378 <bakhtiarniayahya1378@gmail.com>
2021-06-12 01:41:10 -0700

    Initial Commit

---
+++ Slice-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slice-iOS
+//
+//  Created by Yahya Bn on 6/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
