4fa28b73b3d3a00422ae0d851bedcf82a2239b5e
bakhtiarniayahya1378 <bakhtiarniayahya1378@gmail.com>
2021-06-12 14:23:56 -0700

    Add Login Screen and Some Basic Details from Home Screen

---
+++ Slice-iOS/View/TypeCollectionCell/TypeCollectionCell.swift
@@ -0,0 +1,17 @@
+//
+//  TypeCollectionCell.swift
+//  Slice-iOS
+//
+//  Created by Yahya Bn on 6/12/21.
+//
+
+import UIKit
+
+class TypeCollectionCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
